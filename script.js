a6e8160627c187809db9cfb31d7df82bd81be5be
ofirlevy6658 <ofirlevy6658@gmail.com>
2021-03-05 16:19:08 +0200

    new info

--- script.js
+++ script.js
@@ -2,9 +2,6 @@
 // async function getContinent(continent) {
 // 	const resposne = await fetch(
 // 		`https://cors-anywhere.herokuapp.com/https://restcountries.herokuapp.com/api/v1/region/${continent}`
-// 	);
-// 	return resposne.json();
-// }
 
 async function getCovid() {
 	const resposne = await fetch(`http://corona-api.com/countries`);
@@ -15,13 +12,9 @@ async function getCountry() {
 	const resposne = await fetch(`https://restcountries.herokuapp.com/api/v1/`);
 	return resposne.json();
 }
-// let asia = [];
-// let europe = [];
-// let africa = [];
-// let america = [];
-// let oceania = [];
-let world = [];
 
+let world = [];
+const worldData = [];
 document.addEventListener("DOMContentLoaded", getData);
 async function getData() {
 	const countries = await getCountry(); // array of countries
@@ -32,42 +25,67 @@ async function getData() {
 				world[i].region = countries[j].region;
 		}
 	}
-	// world.sort((a, b) => (a.region >= b.region ? 1 : -1)); //sort by continent
-	// //place 4 to 63 is africa , 63 to  119 is america , 120 to 169 is asisa , 170 to 221 is europ 222 to 248 is oceania
-	// africa = world.slice(4, 62);
-	// america = world.slice(63, 120);
-	// asia = world.slice(121, 170);
-	// europe = world.slice(171, 222);
-	// oceania = world.slice(223);
-	// console.log(africa);
+	//taking the neccery data and push it into worldData -array of objects
+	world.forEach((country) => {
+		worldData.push({
+			name: country.name.toLowerCase(),
+			region: country.region,
+			deaths: country.latest_data.deaths,
+			confirmed: country.latest_data.confirmed,
+			recovered: country.latest_data.recovered,
+			critical: country.latest_data.critical,
+		});
+	});
 }
-
+const infoBtns = document.querySelectorAll(".info");
+infoBtns.forEach((b) => b.addEventListener("click", infoBottonState));
 const continentBtns = document.querySelectorAll(".continent");
-continentBtns.forEach((b) => b.addEventListener("click", CreateGraph));
+continentBtns.forEach((b) =>
+	b.addEventListener("click", continentBottonsState)
+);
 //
-async function CreateGraph(e) {
+
+async function displayGraph() {
+	document.querySelector(".infoBtn").style.visibility = "visible";
 	document.querySelector(".myChart").remove();
 	const chartElement = document.createElement("canvas");
 	chartElement.classList.add("myChart");
 	document.body.prepend(chartElement);
-	const button = e.target;
-	continentBtns.forEach((btn) => btn.setAttribute("data-use", "false")); //we remove the use from all btns
-	button.setAttribute("data-use", "true"); // we set the dseire btn attribute in use
 	let ctx = document.querySelector(".myChart").getContext("2d");
-	let graph = updateGraph(button.id);
+	let graph = createGraph();
 	let chart = new Chart(ctx, graph);
-	chart.update(graph);
 }
 
-function updateGraph(graphLabel) {
-	let labels = [];
-	let data = [];
-	world.forEach((country) => {
+function continentBottonsState(e) {
+	continentBtns.forEach((btn) => btn.setAttribute("data-use", "false")); //we remove the use from all btns
+	e.target.setAttribute("data-use", "true"); // we set the dseire btn attribute in use
+	displayGraph();
+}
+function infoBottonState(e) {
+	infoBtns.forEach((btn) => btn.setAttribute("data-use", "false"));
+	e.target.setAttribute("data-use", "true");
+	displayGraph();
+}
+
+function updateGraph2(graphLabel, dataType) {
+	const labels = [];
+	const data = [];
+	worldData.forEach((country) => {
 		if (country.region.toLowerCase() == graphLabel) {
 			labels.push(country.name);
-			data.push(country.latest_data.deaths);
+			data.push(country[dataType]);
 		}
 	});
+	return [labels, data];
+}
+
+function createGraph() {
+	const state = document.querySelectorAll('[data-use="true"]');
+	const dataType = state[0].id;
+	const graphLabel = state[1].id;
+	const graphColor = graphColors(dataType);
+	console.log(graphColor);
+	const graphData = updateGraph2(graphLabel, dataType);
 	const graph = {
 		type: "line",
 		data: {
@@ -75,25 +93,32 @@ function updateGraph(graphLabel) {
 			datasets: [
 				{
 					label: "",
-					backgroundColor: "rgb(255, 99, 132)",
-					borderColor: "rgb(255, 99, 132)",
+					backgroundColor: graphColor[0],
+					borderColor: graphColor[1],
 					data: [],
 				},
 			],
 		},
 	};
+
 	graph.data.datasets[0].label = `${
 		graphLabel.charAt(0).toUpperCase() + graphLabel.slice(1)
-	}`;
-	graph.data.labels.push(...labels);
-	graph.data.datasets[0].data.push(...data);
+	} ${dataType}`;
+	graph.data.labels.push(...graphData[0]);
+	graph.data.datasets[0].data.push(...graphData[1]);
 	return graph;
 }
 
-function removeData(chart) {
-	chart.data.labels.pop();
-	chart.data.datasets.forEach((dataset) => {
-		dataset.data.pop();
-	});
-	chart.update();
+function graphColors(dataType) {
+	console.log(dataType);
+	switch (dataType) {
+		case "confirmed":
+			return ["#ffa64c", "#ff8000"];
+		case "deaths":
+			return ["#ff9999", "#ff0000"];
+		case "recovered":
+			return ["#EBF7E3", "#9BD770"];
+		case "critical":
+			return ["#ffeb3b", "black"];
+	}
 }
